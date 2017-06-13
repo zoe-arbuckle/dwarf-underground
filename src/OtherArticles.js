85e808aa62437d0175d38e1a57ca5e10f7a98784
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-13 19:58:58 -0400

    Create new component for the articles at the bottom of the page

--- src/OtherArticles.js
+++ src/OtherArticles.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import './OtherArticles.css'
+import ClickbaitArticle from './ClickbaitArticle'
 
 class OtherArticles extends Component {
     render() {
         return (
             <div className="small-12 columns other-articles">
                 <h2>From around the Realm</h2>
-                <div className="small-6 medium-3 columns other-article">
-                    <a href="#">
-                        <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-                        <p>Single Orcs in Indianapolis</p>
-                    </a>
-                </div>
+                <ClickbaitArticle />
                 <div className="small-6 medium-3 columns other-article">
                     <a href="#">
                         <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
