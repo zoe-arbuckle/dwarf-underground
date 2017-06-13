4abced619c790c033057cc66284e12f6679ff447
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-13 18:17:06 -0400

    Create an article component in main

--- src/Main.js
+++ src/Main.js
@@ -1,45 +1,12 @@
 import React, { Component } from 'react'
 import './Main.css'
+import Article from './Article'
 
 class Main extends Component {
     render() {
         return (
             <main className="expanded row">
-                <div className="large-8 medium-12 columns article">
-                    <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-                    <div className="avatar">
-                        <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-                        <div className="author-info">
-                            <p className="author-name">By Domri, son of Flug</p>
-                            <p className="date">on 28 April, 3018 of the Third Age</p>
-                        </div>
-                    </div>
-                    <section className="article-body">
-                        <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-                        <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-
-                        <p>Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.</p>
-
-                        <p className="interject">Avast! A stray goblin!</p>
-
-                        <p>Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.</p>
-
-                        <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
-                        <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-                    </section>
-                    <div className="article-links">
-                        <a className="article-link" href="#">
-                            <i className="fa fa-comments-o"></i>
-                            <span className="article-link-text">Comments</span>
-                        </a>
-                        <a className="article-link" href="#">
-                            <i className="fa fa-share"></i>
-                            <span className="article-link-text">Share Post</span>
-                        </a>
-                    </div>
-                </div>
+                <Article />
                 <aside className="large-4 medium-12 columns">
                     <div className="ad">
                         <h3>The Last Hammer You'll Ever Need</h3>
