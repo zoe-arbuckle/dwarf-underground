81432f81e99b6eef0cdca97dd21c9f9ce54257da
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-13 19:28:06 -0400

    Move article links into separate component

--- src/Article.js
+++ src/Article.js
@@ -1,39 +1,31 @@
 import React, { Component } from 'react'
 import './Article.css'
 import Avatar from './Avatar'
+import ArticleLinks from './ArticleLinks'
 
 class Article extends Component {
     render() {
         return (
-                <div className="large-8 medium-12 columns article">
-                    <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-                    <Avatar />
-                    <section className="article-body">
-                        <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+            <div className="large-8 medium-12 columns article">
+                <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+                <Avatar />
+                <section className="article-body">
+                    <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
 
-                        <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+                    <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
 
-                        <p>Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.</p>
+                    <p>Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.</p>
 
-                        <p className="interject">Avast! A stray goblin!</p>
+                    <p className="interject">Avast! A stray goblin!</p>
 
-                        <p>Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.</p>
+                    <p>Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.</p>
 
-                        <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+                    <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
 
-                        <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
-                    </section>
-                    <div className="article-links">
-                        <a className="article-link" href="#">
-                            <i className="fa fa-comments-o"></i>
-                            <span className="article-link-text">Comments</span>
-                        </a>
-                        <a className="article-link" href="#">
-                            <i className="fa fa-share"></i>
-                            <span className="article-link-text">Share Post</span>
-                        </a>
-                    </div>
-                </div>
+                    <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+                </section>
+                <ArticleLinks />
+            </div>
         )
     }
 }
---
+++ src/ArticleLinks.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './ArticleLinks.css'
+
+class ArticleLinks extends Component {
+    render() {
+        return (
+            <div className="article-links">
+                <a className="article-link" href="#">
+                    <i className="fa fa-comments-o"></i>
+                    <span className="article-link-text">Comments</span>
+                </a>
+                <a className="article-link" href="#">
+                    <i className="fa fa-share"></i>
+                    <span className="article-link-text">Share Post</span>
+                </a>
+            </div>
+        )
+    }
+}
+
+export default ArticleLinks
