6d3397bdb2c7ca47a630650583458c76f99f20ac
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-14 14:35:06 -0400

    Handle comment updates

--- src/Comments.js
+++ src/Comments.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 class Comments extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            comment: ''
+        }
+
+        this.updateComment = this.updateComment.bind(this)
+    }
+
+    updateComment(e){
+        this.setState({comment: e.target.value}, () => console.log(this.state))
+    }
+    
     render(){
         return(
             <div className="comments">
-                <textarea></textarea>
+                <textarea value={this.state.comment} onChange={this.updateComment}></textarea>
                 <button className="button">Comment</button>
             </div>
         )
